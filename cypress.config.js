487bf3db3d45ebde26687ec91eb50bb23516d710
timkrysta <krystatymoteusz@gmail.com>
2023-10-26 13:22:43 +0200

    Configured the baseUrl property

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://angular-qa-recruitment-app.netlify.app',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
